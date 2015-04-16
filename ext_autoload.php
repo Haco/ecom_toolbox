c9752ce725a7eacf9acc170176bb468204d9fb01
Sebastian Iffland <sebastian.iffland@ecom-ex.com>
2015-04-16 15:53:38 +0200

    registered inArray VH

--- ext_autoload.php
+++ ext_autoload.php
@@ -4,5 +4,6 @@
 	return array(
 		'TYPO3\CMS\Fluid\ViewHelpers\Link\TypolinkViewHelper' => $extensionClassesPath . 'ViewHelpers/Link/TypolinkViewHelper.php',
 		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\Format\BytesViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/Format/BytesViewHelper.php',
+		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\InArrayViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/InArrayViewHelper.php',
 		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\NegateViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/NegateViewHelper.php'
 	);
