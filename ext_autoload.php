977671df641fd1b9d295a27955b01e71c4e9c684
Scheidler, Nicolas <Nicolas.Scheidler@ecom-ex.com>
2015-05-20 14:40:16 +0200

    Added Timespan ViewHelper
    Calculate Timespan between two Dates (timestamp[int] | DateTime Object)
    with optional rounding.
    
    Returns timespan as days or timestamp.

--- ext_autoload.php
+++ ext_autoload.php
@@ -15,5 +15,6 @@
 		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\IsIntegerViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/IsIntegerViewHelper.php',
 		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\MakeListViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/MakeListViewHelper.php',
 		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\NegateViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/NegateViewHelper.php',
-		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\ParseUrlViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/ParseUrlViewHelper.php'
+		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\ParseUrlViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/ParseUrlViewHelper.php',
+		'TYPO3\CMS\Fluid\ViewHelpers\Ecom\TimespanViewHelper' => $extensionClassesPath . 'ViewHelpers/Ecom/TimespanViewHelper.php'
 	];
