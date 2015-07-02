9aa3eb3a2c630c490a50aa3b90ddba024ea3e109
S3b0 <Sebastian.Iffland@gmx.de>
2015-07-02 11:30:29 +0200

    BitHandler Utility added
    ported from ecompc

---
+++ Classes/Utility/BitHandler.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebo
+ * Date: 14.10.14
+ * Time: 10:12
+ */
+
+namespace Ecom\EcomToolbox\Utility;
+
+/**
+ * Class BitHandler
+ *
+ * @package S3b0\Ecompc\Utility
+ */
+class BitHandler {
+
+	/**
+	 * @var integer
+	 */
+	protected $bits = 0;
+
+	/**
+	 * Gets the bits
+	 *
+	 * @return int
+	 */
+	public function getBits() {
+		return $this->bits;
+	}
+
+	/**
+	 * Sets the bits
+	 *
+	 * @param $bits
+	 */
+	public function setBits($bits) {
+		$this->bits = $bits;
+	}
+
+	/**
+	 * Resets the bits property
+	 *
+	 * @return $this For method chaining
+	 */
+	public function reset() {
+		$this->setBits(0);
+		return $this;
+	}
+
+	/**
+	 * Checks if flag (bit) is set
+	 *
+	 * @param  int  $bit The Bit to be checked against
+	 * @return bool
+	 */
+	public function isBitSet($bit) {
+		return ($this->bits & $bit) == $bit;
+	}
+
+	/**
+	 * @param int $bit The Bit to be set
+	 */
+	public function setSingleBit($bit = 0) {
+		$this->bits |= $bit;
+	}
+
+	/**
+	 * @param int $bit The Bit to be unset
+	 */
+	public function unsetSingleBit($bit = 0) {
+		$this->bits &= ~$bit;
+	}
+
+}
