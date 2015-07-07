1ed1121cbc2b3d6cfea18204f0b5e63c97143ea6
S3b0 <Sebastian.Iffland@gmx.de>
2015-07-07 14:46:50 +0200

    Validators added ; avoid devlog messages

---
+++ Classes/Domain/Validator/StateValidator.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: S3b0
+	 * Date: 07/07/15
+	 * Time: 2:36 PM
+	 */
+
+	namespace Ecom\EcomToolbox\Domain\Validator;
+
+
+	class StateValidator extends \TYPO3\CMS\Extbase\Validation\Validator\GenericObjectValidator {}
