98d2f0c10741fa2a69d9b8bd5dc763f5dee4bd39
S3b0 <Sebastian.Iffland@gmx.de>
2015-12-01 15:49:24 +0100

    Added Model User

---
+++ Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,44 @@
+<?php
+
+$columns = [
+	'ecom_toolbox_country' => [
+		'exclude' => 1,
+		'label' => 'LLL:EXT:ecom_toolbox/Resources/Private/Language/locallang_db.xlf:fe_users.ecom_toolbox_country',
+		'config' => [
+			'type' => 'select',
+			'items' => [
+				['']
+			],
+			'foreign_table' => 'tx_ecomtoolbox_domain_model_region',
+			'foreign_table_where' => 'AND tx_ecomtoolbox_domain_model_region.type=0 AND tx_ecomtoolbox_domain_model_region.deleted=0 ORDER BY tx_ecomtoolbox_domain_model_region.title',
+			'minitems' => 1
+		]
+	],
+	'ecom_toolbox_state' => [
+		'displayCond' => 'FIELD:ecom_toolbox_country:REQ:TRUE',
+		'exclude' => 1,
+		'label' => 'LLL:EXT:ecom_toolbox/Resources/Private/Language/locallang_db.xlf:fe_users.ecom_toolbox_state',
+		'config' => [
+			'type' => 'select',
+			'items' => [
+				[ '', 0 ]
+			],
+			'foreign_table' => 'tx_ecomtoolbox_domain_model_state',
+			'foreign_table_where' => 'AND tx_ecomtoolbox_domain_model_state.country=###REC_FIELD_ecom_toolbox_country### AND tx_ecomtoolbox_domain_model_state.deleted=0 ORDER BY tx_ecomtoolbox_domain_model_state.title'
+		]
+	],
+	'privacy_policy' => [
+		'exclude' => 1,
+		'label' => 'LLL:EXT:ecom_toolbox/Resources/Private/Language/locallang_db.xlf:fe_users.privacy_policy',
+		'config' => [
+			'type' => 'check',
+			'default' => 1
+		]
+	]
+];
+
+$GLOBALS[ 'TCA' ][ 'fe_users' ][ 'ctrl' ][ 'requestUpdate' ] .= ',ecom_toolbox_country';
+$GLOBALS[ 'TCA' ][ 'fe_users' ][ 'palettes' ][ 'ecomtoolbox.0' ] = [ 'showitem' => 'country' ] ;
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns( 'fe_users', $columns, true );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes( 'fe_users', 'ecom_toolbox_country;;ecomtoolbox.0, ecom_toolbox_state, privacy_policy', '', 'replace:country' );
--- ext_tables.sql
+++ ext_tables.sql
@@ -186,3 +186,13 @@ CREATE TABLE sys_category (
 CREATE TABLE sys_file_reference (
 	hideinpost tinyint(4) DEFAULT '0' NOT NULL
 );
+
+#
+# Table structure for table 'fe_users'
+#
+CREATE TABLE fe_users (
+	gender int(11) default '-1',
+	ecom_toolbox_country int(11) unsigned DEFAULT '0',
+	ecom_toolbox_state int(11) unsigned DEFAULT '0',
+	privacy_policy tinyint(4) unsigned DEFAULT '0' NOT NULL
+);
